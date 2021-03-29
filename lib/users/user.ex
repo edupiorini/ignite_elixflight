3284ad68440de21691d8e2a9739fac9255ef27d9
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-29 04:54:30 -0300

    user defined

---
+++ lib/users/user.ex
@@ -0,0 +1,20 @@
+defmodule IgniteElixflight.Users.User do
+  @keys [:id, :name, :email, :cpf]
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(name, email, cpf) when is_bitstring(cpf) do
+    id = UUID.uuid4()
+
+    {:ok,
+     %__MODULE__{
+       id: id,
+       name: name,
+       email: email,
+       cpf: cpf
+     }}
+  end
+
+  def build(_name, _email, _cpf), do: {:error, "Invalid Parameters!"}
+end
