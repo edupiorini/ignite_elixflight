fa8fec7a522cf299030bf0b052744c2a153cbcb8
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-29 06:21:30 -0300

    booking struct created and generating. Validations pending

---
+++ lib/bookings/booking.ex
@@ -0,0 +1,28 @@
+defmodule IgniteElixflight.Bookings.Booking do
+  alias IgniteElixflight.Bookings.Date
+  alias IgniteElixflight.Users.User
+
+  @keys [:id, :data_completa, :cidade_origem, :cidade_destino, :id_usuario]
+  @enforce_keys @keys
+
+  defstruct @keys
+
+  def build(
+        %User{id: id_usuario},
+        %Date{year: year, month: month, day: day, hour: hour, minute: minute, second: second},
+        cidade_origem,
+        cidade_destino
+      ) do
+    uuid = UUID.uuid4()
+    data_completa = NaiveDateTime.new!(year, month, day, hour, minute, second)
+
+    {:ok,
+     %__MODULE__{
+       id: uuid,
+       data_completa: data_completa,
+       cidade_destino: cidade_destino,
+       cidade_origem: cidade_origem,
+       id_usuario: id_usuario
+     }}
+  end
+end
