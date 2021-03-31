a8f491b0fc4bc06b630caefa5a5aaa396d445b75
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-31 06:00:12 -0300

    create update user test ok

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -13,6 +13,10 @@ defmodule IgniteElixflight.Factory do
     }
   end
 
+  def params_factory do
+    %{name: "Eduardo Teste", email: "teste@email.com", cpf: "12345678900"}
+  end
+
   def data_factory do
     %Date{year: 2021, month: 02, day: 22, hour: 20, minute: 10, second: 0}
   end
---
+++ test/users/create_update_test.exs
@@ -0,0 +1,34 @@
+defmodule IgniteElixflightTest.CreateUpdateTest do
+  use ExUnit.Case
+
+  alias IgniteElixflight.Users.Agent, as: UserAgent
+  alias IgniteElixflight.Users.CreateUpdate, as: CreateUpdateUser
+
+  import IgniteElixflight.Factory
+
+  describe "call/1" do
+    setup do
+      UserAgent.start_link("")
+
+      cpf = "12345678900"
+
+      {:ok, cpf: cpf}
+    end
+
+    test "given valid parameters, creates an user" do
+      params = build(:params)
+
+      response = CreateUpdateUser.call(params)
+
+      assert response == {:ok, "Success! User created"}
+    end
+
+    test "given invalid paramaters, returns an error" do
+      params = build(:params, cpf: 000)
+
+      response = CreateUpdateUser.call(params)
+
+      assert response == {:error, "Invalid Parameters!"}
+    end
+  end
+end
